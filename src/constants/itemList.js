981fa6790d853e4f33805fab4e475f4430628d86
CkickenOnFloor <jawwadsiddique0987@gmail.com>
2026-08-11 20:24:04 +0500

    Finally Done

--- src/constants/itemList.js
+++ src/constants/itemList.js
@@ -1,4 +1,4 @@
-const INITIAL_DATA = [
+export default INITIAL_DATA = [
   { class: "Pet", name: "1x1x1x1", maxPrice: "1m" },
   { class: "Lootbox", name: "2023 Large Christmas Present", maxPrice: "1m" },
   { class: "Lootbox", name: "2023 Medium Christmas Present", maxPrice: "1m" },
